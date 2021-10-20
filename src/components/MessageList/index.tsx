ca9b23463e747a1f0dd86a06a5de6fe31868a9a4
Gustavo <gustavo.prog40@gmail.com>
2021-10-20 13:47:32 -0300

    indo-2

--- src/components/MessageList/index.tsx
+++ src/components/MessageList/index.tsx
@@ -4,7 +4,47 @@ import logoImg from '../../assets/logo.svg'
 export function MessageList() {
   return (
     <div className={styles.messageListWrapper}>
-      <img src={logoImg} alt="logo" />
+      <img src={logoImg} alt="logo" style={{ paddingBottom: '80px' }} />
+
+      <ul className={styles.messageList}>
+        <li className={styles.message}>
+          <p className={styles.messageContent}>
+            Texto da mensagem com algoritmosTexto da mensagem com algoritmos Texto da mensagem com
+            algoritmosTexto da mensagem com algoritmos Texto da mensagem com algoritmos
+          </p>
+          <div className={styles.messageUser}>
+            <div className={styles.userImage}>
+              <img src="https://github.com/gustavoFSprog.png" alt=" Gustavo Sohne" />
+            </div>
+            <span>Diego Fernandes</span>
+          </div>
+        </li>
+        <li className={styles.message}>
+          <p className={styles.messageContent}>
+            Texto da mensagem com algoritmosTexto da mensagem com algoritmos Texto da mensagem com
+            algoritmosTexto da mensagem com algoritmos Texto da mensagem com algoritmos
+          </p>
+          <div className={styles.messageUser}>
+            <div className={styles.userImage}>
+              <img src="https://github.com/gustavoFSprog.png" alt=" Gustavo Sohne" />
+            </div>
+            <span>Diego Fernandes</span>
+          </div>
+        </li>
+        <li className={styles.message}>
+          <p className={styles.messageContent}>
+            {' '}
+            Texto da mensagem com algoritmosTexto da mensagem com algoritmos Texto da mensagem com
+            algoritmosTexto da mensagem com algoritmos Texto da mensagem com algoritmos
+          </p>
+          <div className={styles.messageUser}>
+            <div className={styles.userImage}>
+              <img src="https://github.com/gustavoFSprog.png" alt=" Gustavo Sohne" />
+            </div>
+            <span>Diego Fernandes</span>
+          </div>
+        </li>
+      </ul>
     </div>
   )
 }
