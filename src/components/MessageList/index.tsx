9758a5aa2d077a79c4bfd787259861c041aa8dd5
Gustavo <gustavo.prog40@gmail.com>
2021-10-20 14:23:58 -0300

    finalmente

--- src/components/MessageList/index.tsx
+++ src/components/MessageList/index.tsx
@@ -1,50 +1,49 @@
 import styles from './styles.module.scss'
 import logoImg from '../../assets/logo.svg'
+import api from '../../services/apí'
+import { useEffect, useState } from 'react'
+
+type Message = {
+  id: string
+  text: string
+  user: {
+    name: string
+    avatar_url: string
+  }
+}
 
 export function MessageList() {
+  const [messages, setMessages] = useState<Message[]>([])
+
+  async function getAll() {
+    const { data } = await api.get<Message[]>('/messages/last3')
+
+    setMessages(data)
+  }
+
+  useEffect(() => {
+    getAll()
+  }, [])
+
   return (
     <div className={styles.messageListWrapper}>
       <img src={logoImg} alt="logo" style={{ paddingBottom: '80px' }} />
 
-      <ul className={styles.messageList}>
-        <li className={styles.message}>
-          <p className={styles.messageContent}>
-            Texto da mensagem com algoritmosTexto da mensagem com algoritmos Texto da mensagem com
-            algoritmosTexto da mensagem com algoritmos Texto da mensagem com algoritmos
-          </p>
-          <div className={styles.messageUser}>
-            <div className={styles.userImage}>
-              <img src="https://github.com/gustavoFSprog.png" alt=" Gustavo Sohne" />
-            </div>
-            <span>Diego Fernandes</span>
-          </div>
-        </li>
-        <li className={styles.message}>
-          <p className={styles.messageContent}>
-            Texto da mensagem com algoritmosTexto da mensagem com algoritmos Texto da mensagem com
-            algoritmosTexto da mensagem com algoritmos Texto da mensagem com algoritmos
-          </p>
-          <div className={styles.messageUser}>
-            <div className={styles.userImage}>
-              <img src="https://github.com/gustavoFSprog.png" alt=" Gustavo Sohne" />
-            </div>
-            <span>Diego Fernandes</span>
-          </div>
-        </li>
-        <li className={styles.message}>
-          <p className={styles.messageContent}>
-            {' '}
-            Texto da mensagem com algoritmosTexto da mensagem com algoritmos Texto da mensagem com
-            algoritmosTexto da mensagem com algoritmos Texto da mensagem com algoritmos
-          </p>
-          <div className={styles.messageUser}>
-            <div className={styles.userImage}>
-              <img src="https://github.com/gustavoFSprog.png" alt=" Gustavo Sohne" />
-            </div>
-            <span>Diego Fernandes</span>
-          </div>
-        </li>
-      </ul>
+      {messages.map((items) => {
+        return (
+          <ul className={styles.messageList}>
+            <li className={styles.message}>
+              <p className={styles.messageContent}>{items.text}</p>
+              <div className={styles.messageUser}>
+                <div className={styles.userImage}>
+                  <img src="https://github.com/gustavoFSprog.png" alt=" Gustavo Sohne" />
+                </div>
+                <span>{items.user.name}</span>
+              </div>
+            </li>
+          </ul>
+        )
+      })}
     </div>
   )
 }
